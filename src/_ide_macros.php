ff959af117fc02266317b4ba1f31335a1024239e
Dmytro Gural <268188117+DmytroGural@users.noreply.github.com>
2026-07-24 05:09:53 +0200

    Create _ide_macros.php
    chore: add IDE helper stub for Request::location() macro

---
+++ src/_ide_macros.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Http {
+    /**
+     * @method \Stevebauman\Location\Position|bool location(?string $ip = null)
+     */
+    class Request
+    {
+        
+    }
+}
